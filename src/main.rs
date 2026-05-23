974dceef2b0939f38dc37da7ddeefb438c55c999
Vorwes <thabitbustanji@hotmail.com>
2026-05-23 22:24:06 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,24 @@ use std::io::{self, Write};
 use std::process::exit;
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
         command = command.trim().to_string();
+
+        let builtins = &["exit", "echo"];
         if command == "exit" {
             break;
         } else if command.starts_with("echo") {
             println!("{}", &command[5..]);
+        } else if command.starts_with("type") {
+            if builtins.contains(&&command[5..]) {
+                println!("{} is a shell builtin", &command[5..]);
+            } else {
+                println!("{}: not found", &command[5..]);
+            }
         } else {
             println!("{}: command not found", command.trim());
         }
