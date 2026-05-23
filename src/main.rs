d5accf3b59d580d85e2559a619a2cdc333dc93b5
Vorwes <thabitbustanji@hotmail.com>
2026-05-23 21:51:41 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process::exit;
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
@@ -8,6 +9,9 @@ fn main() {
         io::stdout().flush().unwrap();
         let mut command = String::new();
         io::stdin().read_line(&mut command).unwrap();
+        if command.trim() == "exit" {
+            exit(0);
+        }
         println!("{}: command not found", command.trim());
     }
 }
